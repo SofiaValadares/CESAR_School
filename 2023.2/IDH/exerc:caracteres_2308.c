b0a7ae79feee83cce12a7a73b90a554b8f9792d5
SofiaValadares <svc3@cesar.school>
2023-08-23 10:48:52 -0300

    23/08/2023

---
+++ 2023.2/IDH/exerc:caracteres_2308.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   /*unsigned char x;
+    for(x=0; x<255; x++)
+        printf("%d -> %c\n", x, x);*/
+
+    char y;
+    for(y=-128; y<127; y++)
+        printf("%d -> %c\n", y, y);
+    
+        
+}
